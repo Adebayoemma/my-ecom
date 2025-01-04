a30ec5a5b3acf0ab1b2dfa840587acf0bad18ee9
Adebayoemma <120459142+Adebayoemma@users.noreply.github.com>
2025-01-03 18:06:21 -0800

    added headbar and icons

---
+++ src/component/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Headingbar from "./heading-bar/heading-bar";
+
+function App() {
+  return (
+    <div>
+      <Headingbar />
+    </div>
+  );
+}
+
+export default App;
