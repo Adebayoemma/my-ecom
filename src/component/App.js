c3b1816101e04854f83ac9ff68a24bf8396d4757
Adebayoemma <120459142+Adebayoemma@users.noreply.github.com>
2025-01-11 15:52:51 -0800

    added more page

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./App.css";
 import Headingbar from "./heading-bar/heading-bar";
+import Nav from "./nav/navbar";
+import Promotional from "./promotional/promotional";
+import Featured from "./featured/featured";
+import Leatest from "./leatest/leatest";
+import Shoper from "./shoper/shoper";
 
 function App() {
   return (
     <div>
       <Headingbar />
+      <Nav />
+      <Promotional />
+      <Featured />
+      <Leatest />
+      <Shoper />
     </div>
   );
 }
